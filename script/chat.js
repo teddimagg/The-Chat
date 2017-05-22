ede7ea798cd09758ecae10451581f3e10a14067a
teddimagg <teddi@menn.is>
2017-05-22 16:07:05 +0000

    finalizing a naive injection method and commenting

--- script/chat.js
+++ script/chat.js
@@ -1,15 +1,21 @@
+//Global chatdata variable
 let chatdata = [];
 
+//Refrence and socket on firebasae live database
 let chat = firebase.database().ref().child('chat');
 chat.on('value', snap => {
-    console.log('updating chat');
-    updateChat(snap.val());
+    chatdata = snap.val();
+    updateChat();
 });
 
-function updateChat(snap){
+function updateChat(){
     if(chatdata){
+        //Fetches messageboard from dom an initializes it
         const messageboard = document.querySelector('.messageboard');
-        for(i = chatdata.length; i < snap.length; i++){
+        messageboard.innerHTML = "";
+
+        //Inject message element for every messege in live database
+        chatdata.forEach(single => {
             let message = document.createElement('ul');
                 message.classList.add('menu');
                 message.classList.add('message');
@@ -20,17 +26,17 @@ function updateChat(snap){
                           <img src="https://picturepan2.github.io/spectre/img/avatar-4.png" class="avatar">
                         </div>
                         <div class="tile-content">
-                          ${snap[i].author}
+                          ${single.author}
                         </div>
-                        <b>${snap[i].time} | ${snap[i].date}</b>
+                        <b>${single.time} | ${single.date}</b>
                       </div>
                     </li>
                     <li class="divider"></li>
-                    <p>${snap[i].subject}</p>
+                    <p>${single.subject}</p>
                 `;
+            //Prepending onto messageboard (Backwards FILO)
             messageboard.prepend(message);
-        }
-        chatdata = snap;
+        });
     }
 }
 
@@ -38,18 +44,25 @@ function createMessage(event){
     event.preventDefault();
     //If connection has already been established
     if(chat && event.target.message.value && firebase.auth().currentUser){
+        //Spreading chatdata array on a temporary array and the new message at the end
+        //** Keeping it unessecarily complicated for future purposes **//
         let newdata = [...chatdata, {
             'author': firebase.auth().currentUser.displayName,
             'date': new moment().format('DD.MM.YYYY'),
             'time': new moment().format('HH:mm'),
             'subject': event.target.message.value
         }];
-        newdata = newdata.slice(newdata.length - 20, newdata.length);
+
+        //Slicing the maximum messageboardsize from the collection
+        newdata = newdata.slice((newdata.length > 20) ? newdata.length - 20 : 0, newdata.length);
+        //Updating database from ref
         chat.set(newdata);
 
+        //UI
         event.target.message.classList.remove('is-error');
         event.target.message.value = "";
     } else {
+        //UI
         event.target.message.classList.add('is-error');
     }
 
